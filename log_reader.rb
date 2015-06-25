290b1a070afe0ac47d2b2848359e283e7071c6ad
matugm <matugm@gmail.com>
2015-06-25 16:02:46 +0200

    Fix indentation

--- log_reader.rb
+++ log_reader.rb
@@ -5,20 +5,20 @@ require 'sinatra'
 require 'uri'
 
 class String
-	def filter?
-		encoded = URI.encode(self)
-		path = URI(encoded).path
+  def filter?
+    encoded = URI.encode(self)
+    path = URI(encoded).path
 
-		filter_paths = %w{ /wp-content /wp-admin /wp-includes }
-		filter_paths.any? { |filter| 	path.start_with? filter }
-	end
+    filter_paths = %w{ /wp-content /wp-admin /wp-includes }
+    filter_paths.any? { |filter|   path.start_with? filter }
+  end
 end
 
 def escape_html(str)
-	str = str.gsub("<","&lt;")
-	str = str.gsub(">","&gt;")
-	str = str.gsub("\"","&quot;")
-	str = str.gsub("'", "&#39;")
+  str = str.gsub("<","&lt;")
+  str = str.gsub(">","&gt;")
+  str = str.gsub("\"","&quot;")
+  str = str.gsub("'", "&#39;")
 end
 
 db = Database.new
@@ -30,15 +30,15 @@ get '/' do
 end
 
 get '/code/:num' do
-	data = db.get_code_data(params[:num])
-	data = data.to_a.uniq
-	
-	erb :index, :locals => { :stats => data, :data_types => data_types, :db => db, :time => time } 
+  data = db.get_code_data(params[:num])
+  data = data.to_a.uniq
+
+  erb :index, :locals => { :stats => data, :data_types => data_types, :db => db, :time => time }
 end
 
 get '/:stats' do
-	return "Invalid stat type" unless data_types.include? params[:stats]
-	stats = db.stats(params[:stats])
+  return "Invalid stat type" unless data_types.include? params[:stats]
+  stats = db.stats(params[:stats])
 
-	erb :index, :locals => { :stats => stats, :data_types => data_types, :db => db, :time => time } 
+  erb :index, :locals => { :stats => stats, :data_types => data_types, :db => db, :time => time }
 end
