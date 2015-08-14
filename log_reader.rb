2b058aa6f2289cca26a8c60af3d1eefd7058b1b5
matugm <matugm@gmail.com>
2015-08-15 00:39:31 +0200

    Use 1.9 Hash syntax

--- log_reader.rb
+++ log_reader.rb
@@ -6,7 +6,7 @@ require_relative 'log_database'
 require_relative 'output_filter'
 
 db   = Database.new
-data_types = %w{ user_agent ip code path method referer }
+data_types = %w( user_agent ip code path method referer )
 
 before { @time = Time.now }
 
@@ -18,12 +18,12 @@ get '/code/:num' do
   data = db.get_code_data(params[:num])
   data = data.to_a.uniq
 
-  erb :index, :locals => { :stats => data, :data_types => data_types, :db => db, :time => @time }
+  erb :index, :locals => { stats: data, data_types: data_types, db: db, time: @time }
 end
 
 get '/:stats' do
   return "Invalid stat type" unless data_types.include? params[:stats]
   stats = db.stats(params[:stats])
 
-  erb :index, :locals => { :stats => stats, :data_types => data_types, :db => db, :time => @time }
+  erb :index, :locals => { stats: stats, data_types: data_types, db: db, time: @time }
 end
